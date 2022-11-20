84bbc437ed40cc9e89f5f669cfb575811de32a6f
Salach <pwl.salach@gmail.com>
2022-11-20 16:59:01 +0100

    Refactor aligned with Android Architecture Components

--- app/src/main/java/com/salach/privatescheduler/db/daos/ChoreDao.kt
+++ app/src/main/java/com/salach/privatescheduler/db/daos/ChoreDao.kt
@@ -1,16 +1,19 @@
 package com.salach.privatescheduler.db.daos
 
-import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import com.salach.privatescheduler.db.models.Chore
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ChoreDao {
     @Query("SELECT * FROM Chore")
-    fun getAll(): LiveData<List<Chore>>
+    fun getAll(): Flow<List<Chore>>
 
     @Insert
     suspend fun insertAll(vararg chores: Chore)
+
+    @Query("DELETE FROM Chore")
+    suspend fun deleteAll()
 }
--- app/src/main/java/com/salach/privatescheduler/db/daos/ToDoListDao.kt
+++ app/src/main/java/com/salach/privatescheduler/db/daos/ToDoListDao.kt
@@ -1,16 +1,19 @@
 package com.salach.privatescheduler.db.daos
 
-import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import com.salach.privatescheduler.db.models.ToDoList
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ToDoListDao {
     @Query("SELECT * FROM ToDoList")
-    fun getAll(): LiveData<List<ToDoList>>
+    fun getAll(): Flow<List<ToDoList>>
 
     @Insert
     suspend fun insertAll(vararg toDoLists: ToDoList)
+
+    @Query("DELETE FROM ToDoList")
+    suspend fun deleteAll()
 }
