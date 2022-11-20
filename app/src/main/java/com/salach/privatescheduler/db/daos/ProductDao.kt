07ffd77378b4ddbd66a4b1954cf8bb36db323c15
Salach <pwl.salach@gmail.com>
2022-11-20 17:00:06 +0100

    Refactor aligned with Android Architecture Components

--- app/src/main/java/com/salach/privatescheduler/db/daos/ProductDao.kt
+++ app/src/main/java/com/salach/privatescheduler/db/daos/ProductDao.kt
@@ -3,9 +3,10 @@ package com.salach.privatescheduler.db.daos
 import androidx.room.Dao
 import androidx.room.Query
 import com.salach.privatescheduler.db.models.Product
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ProductDao {
     @Query("SELECT * FROM Product")
-    fun getAll(): List<Product>
+    fun getAll(): Flow<List<Product>>
 }
--- app/src/main/java/com/salach/privatescheduler/db/daos/ShoppingListItemDao.kt
+++ app/src/main/java/com/salach/privatescheduler/db/daos/ShoppingListItemDao.kt
@@ -3,9 +3,10 @@ package com.salach.privatescheduler.db.daos
 import androidx.room.Dao
 import androidx.room.Query
 import com.salach.privatescheduler.db.models.ShoppingListItem
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ShoppingListItemDao {
     @Query("SELECT * FROM ShoppingListItem")
-    fun getAll(): List<ShoppingListItem>
+    fun getAll(): Flow<List<ShoppingListItem>>
 }
